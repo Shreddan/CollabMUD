20f0067d5f52e75587573a03fbf61bd5f82a847b
Shreddan <40694103+Shreddan@users.noreply.github.com>
2019-11-14 19:20:15 +0000

    added Telnet stuff

---
+++ OlcMud/Telnet.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#define NUL			0
+#define ECHO		1
+#define SUPPRGA		3
+#define STATUS		5
+#define TIMINGMARK	6
+#define BEL			7
+#define BS			8
+#define HT			9
+#define LF			10
+#define VT			11
+#define FF			12
+#define CARRET		13
+
+#define MTTS		24
+#define NAWS		31
+
+#define MSDP		69
+#define MSSP		70
+#define MCCP1		85
+#define MCCP2		86
+
+#define MSP			90
+#define MXP			91
+#define ZMP			93
+
+#define ATCP		200
+#define GMCP		201
+             
+#define	XEOF		236	
+#define	SUSP		237	
+#define	ABRT		238	
+#define EOR			239
+#define SE			240
+#define NOP			241
+#define DM			242
+#define BRK			243
+#define IP			244
+#define AO			245
+#define AYT			246
+#define EC			247
+#define EL			248
+#define GA			249
+#define SB			250
+
+#define WILL		251
+#define WONT		252
+#define DO			253
+#define DONT		254
+#define IAC			255
+
