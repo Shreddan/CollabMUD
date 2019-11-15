69c252f9294e6cf278c8a31c0bd6d11276b972a4
Shreddan <40694103+Shreddan@users.noreply.github.com>
2019-11-15 10:59:45 +0000

    added an intro thingy

---
+++ OlcMud/ANSI.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define CSI "\x1b["
+#define GREEN "38;5;46m"
+#define RED "38;5;124m"
+#define YELLOW "38;5;226m"
+#define BLUE "38;5;27m"
+#define CYAN "38;5;51m"
+#define MAGENTA "38;5;164m"
+#define WHITE "38;5;254m"
--- OlcMud/main.cpp
+++ OlcMud/main.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
-#include <iomanip>
+#include "MudEngine.h"
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
 
 int main( int argc, char** argv )
 {
+	MudEngine mud;
 
-	std::cout << "Welcome to the OLCMud v" << VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
+	std::cout << "Welcome to the CollabMud v" << VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
+	std::cout << std::endl;
+
+	mud.Intro();
 
 	getchar();
 
