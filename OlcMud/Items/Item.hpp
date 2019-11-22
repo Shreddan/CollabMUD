c2997db3c3ea6f73737d6e3622464601e1a64293
Shreddan <40694103+Shreddan@users.noreply.github.com>
2019-11-22 13:21:13 +0000

    Rudimentary Item Class - to make virtual at some stage

---
+++ OlcMud/Items/Item.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+enum Material
+{
+	Wood,
+	Stone,
+	Copper,
+	Bronze,
+	Iron,
+	Steel,
+	Anicate,
+	Benalite,
+	Danicrote,
+	Fiskite,
+	Javidite,
+	Stackate,
+	Slicite,
+	Tutasite
+
+};
+
+class Item
+{
+public:
+	Item();
+	~Item();
+
+
+
+
+};
